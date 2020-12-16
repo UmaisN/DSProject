50fd9e428b8209d605f5ef3be19b99e8e927cd21
Umais Nisar <umais.nisar@live.com>
2020-12-16 14:51:04 +0500

    Made Menu

--- DataStructuresProject.cpp
+++ DataStructuresProject.cpp
@@ -1,12 +1,59 @@
-// DataStructuresProject.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+using namespace std;
 
 int main()
 {
-    std::cout << "Hello World!\n";
-    std::cout << "Ibrahim is happy" << std::endl;
+    //Menu
+    int choice = 0;
+    while (choice != 10) {
+        cout << "---------Menu-----------" << endl;
+        cout << "1) Specify the number of machines " << endl;
+        cout << "2) Specify the size of identified space" << endl;
+        cout << "3) Manually assign Id to each machine" << endl;
+        cout << "4) Insert data in the form of key" << endl;
+        cout << "5) Remove data by specifying the key from any machine" << endl;
+        cout << "6) Print the routing table of any machine" << endl;
+        cout << "7) Print the AVL tree on any machine" << endl;
+        cout << "8) Add new machines" << endl;
+        cout << "9) Remove any machine" << endl;
+        cout << "10) Exit" << endl;
+        cout << endl;
+        cout << "Please select an option: ";
+        cin >> choice;
+        if (choice == 1) {
+
+        }
+        else if (choice == 2) {
+
+        }
+        else if (choice == 3) {
+            
+        }
+        else if (choice == 4) {
+                
+        }
+        else if (choice == 5) {
+
+        }
+        else if (choice == 6) {
+
+        }
+        else if (choice == 7) {
+
+        }
+        else if (choice == 8) {
+
+        }
+        else if (choice == 9) {
+                
+        }
+        else if (choice == 10) {
+            break;
+        }
+        else {
+            cout << "Invalid option selected." << endl;
+        }
+    }
 }
 
 
