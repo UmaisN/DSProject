133bd5cc251538373d093ae45ba141f1709fd1cf
Noveen Fatima <noveenramay@gmail.com>
2020-12-21 20:45:39 +0500

    Hashing  function updated

--- Hashing.h
+++ Hashing.h
@@ -5,9 +5,6 @@
 using namespace std;
 
 
-
-
-
 int hex_to_int(char c)
 {
 	int val=0;
@@ -80,98 +77,7 @@ int hex_to_int(char c)
 
 }
 
-string hash_value(string key, int num_bits)  //Can return a hash value of 1-256 bits
-{
-	SHA256 sha256;
-	string hashed_value = sha256(key);
 
-	if (num_bits != 256)
-	{
-		//Over here we are not using a % approach since calculating the denary value of this hash function is not possible with the current library
-		//So simply we will take the number of bits we need
-
-		int num_halfbytes = num_bits / 4;
-		int remaining_bits = num_bits % 4;
-		hashed_value = hashed_value.substr(0, num_halfbytes);
-
-
-		//If the num_bits was not a multiple of 4, we have some bits that we still need to cater to. Their possible values are 0,1,2 or 3
-
-		//In all these scenarios I'm simply dividing the space 0-15 into subspaces (the size depends on the number of remaining bits)
-		//We'll append a value that can be produced in those number of bits that we have left over, at the start of our hashed value to get a hashed_value that is truly that number of bits
-		if (remaining_bits == 1)
-		{
-			int check_value = hex_to_int(hashed_value[0]);
-			if (check_value <= 7)
-			{
-				//Commenting this out because it makes no difference if you add a zero at the start of a number
-				//hashed_value = '0' + hashed_value;
-			}
-			else
-			{
-				hashed_value = '1' + hashed_value;
-			}
-		}
-		else if (remaining_bits == 2)
-		{
-			int check_value = hex_to_int(hashed_value[0]);
-			if (check_value <= 3)
-			{
-				//hashed_value = '0' + hashed_value;
-			}
-			else if(check_value<=7)
-			{
-				hashed_value = '1' + hashed_value;
-			}
-			else if (check_value <= 11)
-			{
-				hashed_value = '2' + hashed_value;
-			}
-			else 
-			{
-				hashed_value = '3' + hashed_value;
-			}
-		}
-		else if (remaining_bits == 3)
-		{
-			int check_value = hex_to_int(hashed_value[0]);
-			if (check_value <= 1)
-			{
-				//hashed_value = '0' + hashed_value;
-			}
-			else if (check_value <= 3)
-			{
-				hashed_value = '1' + hashed_value;
-			}
-			else if (check_value <= 5)
-			{
-				hashed_value = '2' + hashed_value;
-			}
-			else if (check_value <= 7)
-			{
-				hashed_value = '3' + hashed_value;
-			}
-			else if (check_value <= 9)
-			{
-				hashed_value = '4' + hashed_value;
-			}
-			else if (check_value <= 11)
-			{
-				hashed_value = '5' + hashed_value;
-			}
-			else if (check_value <= 13)
-			{
-				hashed_value = '6' + hashed_value;
-			}
-			else
-			{
-				hashed_value = '7' + hashed_value;
-			}
-		}
-	}
-
-	return hashed_value;
-}
 
 //Function to raise an InfInt to a given power
 InfInt InfIntpow(InfInt base_num,InfInt exponent) 
@@ -184,6 +90,25 @@ InfInt InfIntpow(InfInt base_num,InfInt exponent)
 	}
 
 	return result;
+}
+
+//This function converts a hexadecimal string to InfInt
+InfInt convert_hex(string hex)
+{
+	InfInt val = 0;
+	InfInt exponent = 0;
+	InfInt exponentvalue = 0;
+
+	int stringlength = hex.length();
+	stringlength--;
+	for (int i = stringlength; i >= 0; i--)
+	{
+		exponentvalue = InfIntpow(16, exponent);
+		val = val + (exponentvalue * hex_to_int(hex[i]));
+		exponent++;
+	}
+
+	return val;
 }
 
 string int_to_hex(InfInt id)
@@ -265,21 +190,107 @@ string int_to_hex(InfInt id)
 	return hex;
 }
 
-//This function converts a hexadecimal string to InfInt
-InfInt convert_hex(string hex)
-{
-	InfInt val = 0;
-	InfInt exponent = 0;
-	InfInt exponentvalue = 0;
 
-	int stringlength = hex.length();
-	stringlength--;
-	for (int i = stringlength; i >= 0; i--)
+InfInt hash_value(string key, int num_bits)  //Can return a hash value of 1-256 bits
+{
+	SHA256 sha256;
+	string hashed_value = sha256(key);
+	InfInt final_hash = convert_hex(hashed_value);
+
+	if (num_bits != 256)
 	{
-		exponentvalue = InfIntpow(16, exponent);
-		val = val + (exponentvalue * hex_to_int(hex[i]));
-		exponent++;
+		/////////////APROACH 1/////////////
+		//This approach is used when we are returning an InfInt
+		InfInt max_value = InfIntpow(2, num_bits);
+		final_hash = final_hash % max_value;
+
+
+		/////////////APPROACH 2////////////
+		//This approach was used when we were dealing with strings. We were returning a string in hexadecimal representation
+		/*
+		//Over here we are not using a % approach since calculating the denary value of this hash function is not possible with the current library
+		//So simply we will take the number of bits we need
+
+		int num_halfbytes = num_bits / 4;
+		int remaining_bits = num_bits % 4;
+		hashed_value = hashed_value.substr(0, num_halfbytes);
+
+
+		//If the num_bits was not a multiple of 4, we have some bits that we still need to cater to. Their possible values are 0,1,2 or 3
+
+		//In all these scenarios I'm simply dividing the space 0-15 into subspaces (the size depends on the number of remaining bits)
+		//We'll append a value that can be produced in those number of bits that we have left over, at the start of our hashed value to get a hashed_value that is truly that number of bits
+		if (remaining_bits == 1)
+		{
+			int check_value = hex_to_int(hashed_value[0]);
+			if (check_value <= 7)
+			{
+				//Commenting this out because it makes no difference if you add a zero at the start of a number
+				//hashed_value = '0' + hashed_value;
+			}
+			else
+			{
+				hashed_value = '1' + hashed_value;
+			}
+		}
+		else if (remaining_bits == 2)
+		{
+			int check_value = hex_to_int(hashed_value[0]);
+			if (check_value <= 3)
+			{
+				//hashed_value = '0' + hashed_value;
+			}
+			else if(check_value<=7)
+			{
+				hashed_value = '1' + hashed_value;
+			}
+			else if (check_value <= 11)
+			{
+				hashed_value = '2' + hashed_value;
+			}
+			else
+			{
+				hashed_value = '3' + hashed_value;
+			}
+		}
+		else if (remaining_bits == 3)
+		{
+			int check_value = hex_to_int(hashed_value[0]);
+			if (check_value <= 1)
+			{
+				//hashed_value = '0' + hashed_value;
+			}
+			else if (check_value <= 3)
+			{
+				hashed_value = '1' + hashed_value;
+			}
+			else if (check_value <= 5)
+			{
+				hashed_value = '2' + hashed_value;
+			}
+			else if (check_value <= 7)
+			{
+				hashed_value = '3' + hashed_value;
+			}
+			else if (check_value <= 9)
+			{
+				hashed_value = '4' + hashed_value;
+			}
+			else if (check_value <= 11)
+			{
+				hashed_value = '5' + hashed_value;
+			}
+			else if (check_value <= 13)
+			{
+				hashed_value = '6' + hashed_value;
+			}
+			else
+			{
+				hashed_value = '7' + hashed_value;
+			}
+		}
+		*/
 	}
 
-	return val;
+	return final_hash;
 }
